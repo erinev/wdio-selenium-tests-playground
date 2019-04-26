feeefe4d1ae2294b3a029e1111686b31623ed898
Erikas Neverdauskas <e.neverdauskas@adform.com>
2019-04-26 22:39:35 +0300

    added console logs for all forms elements (to find that isEnabled works only for form elements)

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -3,37 +3,62 @@ describe('Describe -', () => {
     browser.url(`file:///${__dirname}/../app/index.html`);
   });
 
-  it('console outputs', () => {
-    const enabledButton = $('a.create-link-enabled');
-    const disabledButton = $('a.create-link-disabled');
+  it.only('console outputs', () => {
+    const disabledLink = $('#link-disabled');
+    const disabledTextInput = $('#text-input-disabled');
+    const disabledButtonInput = $('#button-input-disabled');
+    const disabledSelect = $('#select-disabled');
+    const disabledTextarea = $('#textarea-disabled');
+    const disabledButton = $('#button-disabled');
 
-    console.log(`isEnabled() result for ENABLED button: ${enabledButton.isEnabled()}`);
-    console.log(`isEnabled() result for DISABLED button: ${disabledButton.isEnabled()}`);
+    console.log(`isEnabled() result for DISABLED link:         ${disabledLink.isEnabled()}`);
+    console.log(`isEnabled() result for DISABLED text input:   ${disabledTextInput.isEnabled()}`);
+    console.log(`isEnabled() result for DISABLED button input: ${disabledButtonInput.isEnabled()}`);
+    console.log(`isEnabled() result for DISABLED select:       ${disabledSelect.isEnabled()}`);
+    console.log(`isEnabled() result for DISABLED textarea:     ${disabledTextarea.isEnabled()}`);
+    console.log(`isEnabled() result for DISABLED button:       ${disabledButton.isEnabled()}`);
 
-    const getEnabledButtonAttribute = enabledButton.getAttribute('disabled');
+    const getDisabledLinkAttribute = disabledLink.getAttribute('disabled');
+    const getDisabledTextInputAttribute = disabledTextInput.getAttribute('disabled');
+    const getDisabledButtonInputAttribute = disabledButtonInput.getAttribute('disabled');
+    const getDisabledSelectAttribute = disabledSelect.getAttribute('disabled');
+    const getDisabledTextareaAttribute = disabledTextarea.getAttribute('disabled');
     const getDisabledButtonAttribute = disabledButton.getAttribute('disabled');
 
-    console.log(`getAttribute('disabled') value for ENABLED button: ${getEnabledButtonAttribute}`);
-    console.log(`getAttribute('disabled') value for DISABLED button: ${getDisabledButtonAttribute}`);
+    console.log(`getAttribute('disabled') value for DISABLED link:       ${getDisabledLinkAttribute}`);
+    console.log(`getAttribute('disabled') value for DISABLED text input: ${getDisabledTextInputAttribute}`);
+    console.log(`getAttribute('disabled') value for DISABLED button input:     ${getDisabledButtonInputAttribute}`);
+    console.log(`getAttribute('disabled') value for DISABLED select:           ${getDisabledSelectAttribute}`);
+    console.log(`getAttribute('disabled') value for DISABLED textarea:         ${getDisabledTextareaAttribute}`);
+    console.log(`getAttribute('disabled') value for DISABLED button:           ${getDisabledButtonAttribute}`);
 
     // result:
     /*
-      [0-0] isEnabled() result for ENABLED button: true
-      [0-0] isEnabled() result for DISABLED button: true  <- incorrect because it says true for DISABLED button
-      [0-0] getAttribute('disabled') value for ENABLED button: null
-      [0-0] getAttribute('disabled') value for DISABLED button: true <- correct disabled attribute is detected for DISABLED button
+      [0-0] isEnabled() result for DISABLED link:         true <- doesn't work for disabled link elements (because link is not form element)
+      [0-0] isEnabled() result for DISABLED text input:   false
+      [0-0] isEnabled() result for DISABLED button input: false
+      [0-0] isEnabled() result for DISABLED select:       false
+      [0-0] isEnabled() result for DISABLED textarea:     false
+      [0-0] isEnabled() result for DISABLED button:       false
+
+      [0-0] getAttribute('disabled') value for DISABLED link:         true
+      [0-0] getAttribute('disabled') value for DISABLED text input:   true
+      [0-0] getAttribute('disabled') value for DISABLED button input: true
+      [0-0] getAttribute('disabled') value for DISABLED select:       true
+      [0-0] getAttribute('disabled') value for DISABLED textarea:     true
+      [0-0] getAttribute('disabled') value for DISABLED button:       true
     */
   });
 
   it('wait until disabled button becomes enabled -> using waitForEnabled()', () => {
-    const disabledButton = $('a.create-link-disabled');
+    const disabledButton = $('#link-disabled');
 
     const timeoutMessage = `'${disabledButton.selector}' is not enabled after 5s timeout!`;
     disabledButton.waitForEnabled(5000, false, timeoutMessage);
   });
 
   it('wait until disabled button becomes enabled -> using waitUntil and checking disabled attribute', () => {
-    const disabledButton = $('a.create-link-disabled');
+    const disabledButton = $('#link-disabled');
 
     const waitUntilElementIsNotDisabledCondition = () => !disabledButton.getAttribute('disabled');
 
